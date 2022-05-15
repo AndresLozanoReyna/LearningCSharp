4a0024bbc5d1a1d605ccf8cfbeec79fef4c3e51f
AndresLozanoReyna <98426004+AndresLozanoReyna@users.noreply.github.com>
2022-05-14 23:09:02 -0500

    learningCsharp

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+   class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenidos al curso de C#"); //Esto imprime un mensaje en consola
+            Console.Read();
+        }
+    }
+}
